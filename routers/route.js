20410036a7f8c1842d4da3e50c506e686499c512
qfstudy <qifeistudy@gmail.com>
2019-04-16 21:28:54 +0800

    更新文件

--- routers/route.js
+++ routers/route.js
@@ -2,6 +2,11 @@ const router = require('koa-router')()
 const path= require('path')
 const fs = require('fs')
 
+router.get('/',async (ctx,next) => {
+  ctx.response.type = 'text/html'
+  ctx.response.body = await fs.createReadStream(path.resolve(__dirname, '../index.html'))
+})
+
 router.get('/cnode',async (ctx,next) => {
   ctx.response.type = 'text/html'
   ctx.response.body = await fs.createReadStream(path.resolve(__dirname, '../public/cnodeIndex.html'))
