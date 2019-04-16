9d8aa22a3c2330ddabfa7547f2894072bab9a157
qfstudy <qifeistudy@gmail.com>
2019-04-16 20:36:46 +0800

    修改

--- app.js
+++ app.js
@@ -9,16 +9,9 @@ const app=new Koa()
 
 // 静态资源目录路径
 app.use(static(
-  path.join(__dirname, './cnode')
+  path.join(__dirname, './public')
 ))
 
-// const main = ctx => {
-//   ctx.response.type = 'text/html';
-//   // ctx.response.body = fs.createReadStream(path.resolve(__dirname, './cnode/index.html'))
-//   ctx.response.body = fs.createReadStream('./cnode/index.html');
-// };
-
-// app.use(main)
 
 app.use(require('./routers/route.js').routes())
 
